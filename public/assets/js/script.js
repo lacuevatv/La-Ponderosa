755532db953374c15aa64bac84a0eb0108f11867
coco <coco@lacueva.tv>
2019-10-06 11:19:13 -0300

    comprimi las imagenes las ordene y arme algo de html

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -14,3 +14,13 @@ function initSlider() {
         autoplay: true
     });
 }
+
+function lazyLoadImgs() {
+
+    html = '<picture>';
+        html += '<source srcset="assets/images/galeria/galeria1.jpg 1x, assets/images/galeria1@2x.jpg 2x" media="(min-width: 768px)">';
+        html += '<source srcset="assets/images/galeria/galeria1-mov.jpg 1x, assets/images/galeria1.jpg 2x" media="(min-width: 300px)">';
+        html += '<img src="assets/images/galeria/galeria1.jpg" alt="Fotos La Ponderosa">';
+    html += '</picture>';
+
+}
