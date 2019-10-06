a4b991b91d1263332465a8c582cf7048b7b4c077
coco <coco@lacueva.tv>
2019-10-06 10:09:35 -0300

    agregue el logo y el script del slider

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 HTMl landing page estática 
 
 ## librerias
+* Tiny Slider js 2.9.2 REPO: https://github.com/ganlanyuan/tiny-slider
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 window.addEventListener('load', function() {
     console.log('all loaded');
 });
+
+function initSlider() {
+    var slider = tns({
+        container: '.my-slider',
+        items: 3,
+        slideBy: 'page',
+        autoplay: true
+    });
+}
