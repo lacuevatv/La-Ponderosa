c69fc277ad3baab198e64d3e76f870a81b98cd1d
coco <coco@lacueva.tv>
2019-10-06 23:30:41 -0300

    maquetado la pagina, falta header y nav

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -17,10 +17,58 @@ function initSlider() {
 
 function lazyLoadImgs() {
 
-    html = '<picture>';
-        html += '<source srcset="assets/images/galeria/galeria1.jpg 1x, assets/images/galeria1@2x.jpg 2x" media="(min-width: 768px)">';
-        html += '<source srcset="assets/images/galeria/galeria1-mov.jpg 1x, assets/images/galeria1.jpg 2x" media="(min-width: 300px)">';
-        html += '<img src="assets/images/galeria/galeria1.jpg" alt="Fotos La Ponderosa">';
-    html += '</picture>';
+    var imagenesToLoad = document.querySelectorAll('.lazy-load-image');
 
+    //si no hay ninguna retorna
+    if ( imagenesToLoad.length <= 0 ) {
+        return true;
+    }
+
+    var baseUrl = 'assets/images/galeria/';
+
+    for (var index = 0; index < imagenesToLoad.length; index++) {
+        var imagen = imagenesToLoad[index];
+        
+        var dataImgMovil   = imagen.getAttribute('data-img-mov');
+            dataImgMovil2x = imagen.getAttribute('data-img-mov2x');
+            dataImg        = imagen.getAttribute('data-img');
+            dataImg2x      = imagen.getAttribute('data-img2x');
+
+        if ( dataImgMovil == '' && dataImgMovil2x == '' && dataImg == '' && dataImg2x == '' ) {
+            break;
+        } else {
+
+            html = '<picture>';
+
+            //si hay imagen movil construye el picture por 2 sino solo coloca uno
+            if ( dataImgMovil != '' ) {
+
+                html += '<source srcset="'+ baseUrl + dataImg +' 1x';
+                if ( dataImg2x != '' ) {
+                    html += ', '+ baseUrl + dataImg +' 2x';
+                }
+                html += '" media="(min-width: 768px)">';
+            
+                html += '<source srcset="'+ baseUrl + dataImgMovil +' 1x';
+                if ( dataImgMovil2x != '' ) {
+                    html += ', '+ baseUrl + dataImgMovil2x +' 2x';
+                }
+                html += '" media="(min-width: 300px)">';
+            } else {
+                //al no haber imagen para movil carga una sola source
+                html += '<source srcset="'+ baseUrl + dataImg +' 1x';
+                if ( dataImg2x != '' ) {
+                    html += ', '+ baseUrl + dataImg +' 2x';
+                }
+                html += '" media="(min-width: 300px)">';
+            }
+           
+            //imagen por defecto
+            html += '<img src="'+ baseUrl + dataImg +'" alt="Fotos La Ponderosa">';
+
+            html += '</picture>';
+
+            imagen.innerHTML = html;
+        }//else
+    }//for
 }
