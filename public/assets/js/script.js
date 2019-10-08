8403aa311168aa9ee8f3a3abc17c5167b34ebb29
coco <coco@lacueva.tv>
2019-10-07 23:11:26 -0300

    configurando los script, listos los botones y ocultamientos, faltan las funciones al hacer click

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,90 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Doc Ready');
+    //reduce los textos
+    hideElements();
 });
 
 window.addEventListener('load', function() {
-    console.log('all loaded');
+
+    //activa los sliders
     initSlider();
+
 });
 
+//setea los elementos que hay que cliquear para ver más
+function hideElements() {
+
+    /*
+    * 1. parrafo inicio
+    */
+    
+    var residenciaIntroParagraps = document.querySelector('#residencia').querySelectorAll('p');
+    var btnInicio = document.querySelector('#more-intro');
+
+    for (var index = 1; index < residenciaIntroParagraps.length; index++) {
+        residenciaIntroParagraps[index].style.height = 0;
+    }
+    //cambia título, texto, gira el icono
+    btnInicio.setAttribute('title', 'Más');
+    btnInicio.querySelector('.more-title-btn').innerText = 'Más';
+    btnInicio.querySelector('img').style.transform = 'translateY(-50%) rotate(0)';
+
+    btnInicio.addEventListener('click', function(){
+        openIntro();
+    });
+
+    /*
+    * 2. detalles
+    */
+    var textDetalles = document.querySelectorAll('.text-detalles');
+    var btnDetalles = document.querySelectorAll('.detalles-more-btn');
+    
+    for (var i = 0; i < textDetalles.length; i++) {
+        textDetalles[i].style.height = '50px'; 
+    }
+
+    for (var j = 0; j < btnDetalles.length; j++) {
+        btnDetalles[j].setAttribute('title', 'Leer Más');
+        btnDetalles[j].innerText = 'Leer más';
+        
+        btnDetalles[j].addEventListener('click', function(){
+            openDetails(this);
+        });
+    }
+
+    /*
+    * 3. servicios
+    */
+    var textServicios = document.querySelectorAll('.lista-servicio');
+    var btnServicios = document.querySelectorAll('.service-btn');
+
+    for (var s = 0; s < textDetalles.length; s++) {
+        textServicios[s].style.height = '0'; 
+    }
+    for (var b = 0; b < textDetalles.length; b++) {
+        btnServicios[b].setAttribute('aria-label', 'Ver Más');
+        btnServicios[b].style.transform = 'translateX(-50%) rotate(180deg)';
+
+        btnServicios[b].addEventListener('click', function(){
+            openServicio(this);
+        });
+    }
+    
+}
+
+function openServicio(el) {
+    console.log(el)
+}
+
+function openDetails(el) {
+    console.log(el)
+}
+
+function openIntro() {
+    console.log('inicio')
+}
+
+//inicia el slider
 function initSlider() {
     var slider = tns({
         container: '.slider-content',
